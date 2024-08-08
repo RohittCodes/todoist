0207e9a2807050851637a7db7891349f0cd1a8f7
rohittcodes <rohittcodes@gmail.com>
2024-08-08 21:52:17 +0530

    bug fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
   plugins: [react()],
-})
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      output: {
+        format: 'es',
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+      },
+    },
+  },
+});
